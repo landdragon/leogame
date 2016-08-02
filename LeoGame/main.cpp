231be325f78704ba010e2152913bfa896e5fd515
guirad <guirado.maxime@gmail.com>
2016-08-02 17:08:39 +0200

    ajouter la possibilité de se mouvoir

--- LeoGame/main.cpp
+++ LeoGame/main.cpp
@@ -5,13 +5,41 @@ using namespace std;
 void main()
 {
 	char * deplacement = new char[8];
+	bool EstCeQueJeContinue = true;
 
-	while (strcmp (deplacement,"quitter") != 0 )
+
+	while (EstCeQueJeContinue)
 	{
 		cout << "ou voulez vous aller ?" << endl << "(haut, bas, gauche, droite, quitter)" << endl;
 		cin >> deplacement;
-		cout << endl << endl;
+
+		if (strcmp(deplacement, "haut") == 0)
+		{
+			cout << "vous montez d'une case";
+		}
+		else if (strcmp(deplacement, "bas") == 0)
+		{
+			cout << "vous descendez d'une case";
+		}
+		else if (strcmp(deplacement, "gauche") == 0)
+		{
+			cout << "vous bougez vers la gauche";
+		}
+		else if (strcmp(deplacement, "droite") == 0)
+		{
+			cout << "vous bougez vers la droite";
+		}
+		else if (strcmp(deplacement, "quitter") == 0)
+		{
+			EstCeQueJeContinue = false;
+		}
+		else
+			cout << "veuillez ecrire l'un des choix dans la liste";
+
+			cout << endl << endl;
 	}
+
+
 	delete (deplacement);
 	return;
 }
