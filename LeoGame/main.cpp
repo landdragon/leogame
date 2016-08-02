3b9f2a1b5b3d26386047236804ad9fb2247593cc
guirad <guirado.maxime@gmail.com>
2016-08-02 18:47:25 +0200

    ajout d'un deplacement dans la map

--- LeoGame/main.cpp
+++ LeoGame/main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <windows.h> 
 
 using namespace std;
 
@@ -6,28 +7,91 @@ void main()
 {
 	char * deplacement = new char[8];
 	bool EstCeQueJeContinue = true;
+	int map1[9][9] =  { { 0,0,0,0,0,0,0,0,0 },
+						{ 0,0,0,0,0,0,0,0,0 },
+						{ 0,0,0,0,0,0,0,0,0 },
+						{ 0,0,0,0,0,0,0,0,0 },
+						{ 0,0,0,0,0,0,0,0,0 },
+						{ 0,0,0,0,0,0,0,0,0 },
+						{ 0,0,0,0,0,0,0,0,0 },
+						{ 0,0,0,0,0,0,0,0,0 },
+						{ 0,0,0,0,0,0,0,0,0 } };
+	int x = 1;
+	int y = 7;
 
+
+	
 
 	while (EstCeQueJeContinue)
 	{
+		cout <<"x : "<< x << " , y : " << y <<endl <<endl;
+
 		cout << "ou voulez vous aller ?" << endl << "(haut, bas, gauche, droite, quitter)" << endl;
 		cin >> deplacement;
 
 		if (strcmp(deplacement, "haut") == 0)
 		{
-			cout << "vous montez d'une case";
+			if (x > 0)
+			{
+				x--;
+				cout << "vous montez d'une case";
+			}
+			else
+			{
+				cout << "un magicien vous crie dans les oreilles :";
+				Sleep(1000);
+				cout << " VOUS NE PASSEREZ...";
+				Sleep(1000);
+				cout << " PAS !!!";
+			}
 		}
 		else if (strcmp(deplacement, "bas") == 0)
 		{
-			cout << "vous descendez d'une case";
+			if (x < 8)
+			{
+				x++;
+				cout << "vous descendez d'une case";
+			}
+			else
+			{
+				cout << "un magicien vous crie dans les oreilles :";
+				Sleep(1000);
+				cout << " VOUS NE PASSEREZ...";
+				Sleep(1000);
+				cout << " PAS !!!";
+			}
 		}
 		else if (strcmp(deplacement, "gauche") == 0)
 		{
-			cout << "vous bougez vers la gauche";
+			if (y > 0)
+			{
+				y--;
+				cout << "vous bougez vers la gauche";
+			}
+			else
+			{
+				cout << "un magicien vous crie dans les oreilles :";
+				Sleep(1000);
+				cout << " VOUS NE PASSEREZ...";
+				Sleep(1000);
+				cout << " PAS !!!";
+			}
 		}
 		else if (strcmp(deplacement, "droite") == 0)
 		{
-			cout << "vous bougez vers la droite";
+			if (y < 8)
+			{
+				y++;
+				cout << "vous bougez vers la droite";
+			}
+			else
+			{
+				cout << "un magicien vous crie dans les oreilles :";
+				Sleep(1000);
+				cout << " VOUS NE PASSEREZ...";
+				Sleep(1000);
+				cout << " PAS !!!";
+			}
 		}
 		else if (strcmp(deplacement, "quitter") == 0)
 		{
